33d53c33896449bc411cb6db8dd144dd56b25778
Wesley Lima <wesleyjoga500@gmail.com>
2021-12-21 22:48:39 -0300

    removendo o console.log de teste

--- src/controller/TweetController.js
+++ src/controller/TweetController.js
@@ -1,11 +1,15 @@
-class TweetController {
-    async fetchTweets() {
-        try {
+const TweetsService = require("../services/tweets")
 
+class TweetsController {
+    async fetchTweets(hashtag) {
+        try {
+            const response = await TweetsService.searchTweetByHashtag(hashtag);
+
+            return response;
         } catch (error) {
             throw error;
         }
     }
 }
 
-module.exports = new TweetController;
+module.exports = new TweetsController;
